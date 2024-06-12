2cf24493b7a17dfb107da69603a4b586d76db29d
Your Name <you@example.com>
2024-06-11 22:05:55 -0300

    feat: vercel deploy

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Paths from "paths/paths";
+import { Analytics } from "@vercel/analytics/react";
 
 function App() {
   return (
     <>
       <Paths />
+      <Analytics />
     </>
   );
 }
